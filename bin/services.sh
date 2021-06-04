12ee43d94f4b7fe4cd558268b7b178029ce41f74
Steve Eichblatt <steve.eichblatt@gmail.com>
2021-06-04 12:35:53 -0400

    turn on power feature for v2 boards only

--- bin/services.sh
+++ bin/services.sh
@@ -9,3 +9,14 @@ sudo chgrp root /etc/wpa_supplicant/wpa_supplicant.conf
 sudo systemctl daemon-reload
 sudo systemctl enable timemachine.service 
 sudo systemctl enable connect_network.service
+
+# since this is version 2 branch of the code, make sure that the board is a version 2 board. 
+# If it is not a v2 board, then make it one by adding the gpio-shutdown command.
+#
+version=`$HOME/deadstream/bin/board_version.sh | awk '/version/ {print $2}'`
+if [ $version -ne "2" ]; then
+  echo 'sudo echo "dtoverlay=gpio-shutdown" >> /boot/config.txt'
+  sudo bash -c 'echo "dtoverlay=gpio-shutdown" >> /boot/config.txt'
+fi
+
+exit 0
